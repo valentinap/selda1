22445b175dfe4f29b2eaec22d3f83e74dcbd77e3
valentinap <valentinaporcu@Macintosh-2.local>
2017-11-08 09:29:06 +0100

    v2
    v2

---
+++ 1.1 Come accedere agli esercizi.R
@@ -0,0 +1,19 @@
+# installare il pacchetto swirl
+
+install.packages("swirl")
+
+# richiamare il pacchetto swirl()
+
+library(swirl)
+
+# installare il corso
+
+install_course_github("valentinap", "Introduzione_alla_programmazione_con_R", branch = "master")
+
+# digitiamo 
+swirl()
+
+# il pacchetto vi chiede come vi chiamate
+# inserite il vostro nome
+
+# potete cominciare con le lezioni 
